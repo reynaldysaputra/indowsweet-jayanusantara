1e7e2a78087e82ae9ece7e36249f6ee593809a3e
renalfrontend <renalfrontend@gmail.com>
2025-07-28 10:39:44 +0700

    .

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  reactStrictMode: true
 }
 
 const withNextIntl = createNextIntlPlugin()
